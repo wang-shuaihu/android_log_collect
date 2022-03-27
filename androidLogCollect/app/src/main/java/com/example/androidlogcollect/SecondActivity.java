200dcc495acdc82730acd718e9a87ae45e89c54a
wang-shuaihu <1073283999@qq.com>
2022-03-27 20:36:42 +0800

    android log collect
    Signed-off-by: wang-shuaihu <1073283999@qq.com>

---
+++ androidLogCollect/app/src/main/java/com/example/androidlogcollect/SecondActivity.java
@@ -0,0 +1,39 @@
+package com.example.androidlogcollect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Button button = findViewById(R.id.exitButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+}
